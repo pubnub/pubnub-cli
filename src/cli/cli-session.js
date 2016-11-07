59dc8e8757255acaf962a47ab69a649e8ff10e7d
Max Presman <max@pubnub.com>
2016-11-06 22:30:42 -0800

    start supporting logins

--- src/cli/cli-session.js
+++ src/cli/cli-session.js
@@ -8,7 +8,7 @@ const entryPoint = new EntryPoint({ isCLI: true });
 
 program
   .version(packageInfo.version)
-  .option('create [username] [password]', 'create session, password is optional and can be supplied in runtime.')
+  .option('create [email] [password]', 'create session: email & password is optional and can be supplied in runtime.')
   .parse(process.argv);
 
 const operation = program.rawArgs[2];
@@ -16,9 +16,9 @@ const operation = program.rawArgs[2];
 if (!operation) process.exit(1);
 
 if (operation === 'create') {
-  const username = program.rawArgs[3];
+  const email = program.rawArgs[3];
   const password = program.rawArgs[4];
-  entryPoint.session.create({ username, password });
+  entryPoint.session.create({ email, password });
 }
 
 if (operation === 'delete') {
--- src/components/session.js
+++ src/components/session.js
@@ -1,5 +1,7 @@
 import os from 'os';
 import fs from 'fs';
+import inquirer from 'inquirer';
+import _ from 'lodash';
 
 export default class {
 
@@ -10,9 +12,88 @@ export default class {
     this.interactive = interactive;
   }
 
-  createSession({ username, password }) {
-    return new Promise((resolve) => {
-      resolve({ username, password });
+  createSession({ email, password }) {
+    const inputParams = [
+      {
+        field: email,
+        name: 'email',
+        question: 'Please enter your PubNub email',
+        type: 'input'
+      },
+      {
+        field: password,
+        name: 'password',
+        question: 'Please enter your PubNub password',
+        type: 'password'
+      }
+    ];
+
+    const abstractedPromise = this.createPromise();
+
+    this.abstractedValidator(inputParams).then((fields) => {
+      this.networking.createLoginToken({ email: fields.email, password: fields.password }, (err, serverResponse) => {
+        if (err) {
+          if (this.interactive) this.logger.error(err);
+          else abstractedPromise.reject(err);
+          return;
+        }
+
+        const token = serverResponse.result.token;
+
+        if (this.interactive) {
+          this._createSessionFile(token).then(() => {
+            this.logger.info('Login Succesful, token: ' + token + ' saved to home directory');
+          });
+        }
+      });
+    }).catch((err) => {
+      if (this.interactive) this.logger.error(err);
+      else abstractedPromise.reject(err);
+      return;
+    });
+
+    return abstractedPromise;
+  }
+
+  createPromise() {
+    let successResolve;
+    let failureResolve;
+    const promise = new Promise((fulfill, reject) => {
+      successResolve = fulfill;
+      failureResolve = reject;
+    });
+
+    return { promise, reject: failureResolve, resolve: successResolve };
+  }
+
+  abstractedValidator(params = []) {
+    const response = {};
+    let validationPassing = true; // optimism.
+    const questions = [];
+
+    params.forEach((param) => {
+      if (param.field && _.trim(param.field) !== '') {
+        response[param.name] = _.trim(param.field);
+      } else if (this.interactive) {
+        questions.push({ type: param.type, name: param.name, message: param.question });
+      } else {
+        validationPassing = false;
+      }
+    });
+
+    return new Promise((resolve, reject) => {
+      if (this.interactive) {
+        if (questions.length === 0) return resolve(response);
+
+        inquirer.prompt(questions).then((promptResult) => {
+          Object.assign(response, promptResult);
+          resolve(response);
+        });
+      } else {
+        if (validationPassing) resolve(response);
+        else reject(/* TODO */);
+        return;
+      }
     });
   }
 
@@ -45,7 +126,8 @@ export default class {
   _deleteSessonFile() {
     return new Promise((resolve) => {
       fs.unlink(this.sessionStorage, (err) => {
-        if (err) this.logger.error(err);
+        // silence file not found
+        if (err && err.code !== 'ENOENT') this.logger.error(err);
         resolve();
       });
     });
