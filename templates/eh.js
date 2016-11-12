bca363f75677085748ec6093348de72b2e0f4865
Max Presman <max@pubnub.com>
2016-11-11 17:18:15 -0800

    adding more commits

---
+++ templates/eh.js
@@ -0,0 +1,8 @@
+export default (request) => {
+  const pubnub = require('pubnub');
+  const kvstore = require('kvstore');
+  const xhr = require('xhr');
+
+  console.log(request); // Log the request envelope passed
+  return request.ok(); // Return a promise when you're done
+}
